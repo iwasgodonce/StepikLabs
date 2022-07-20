6052b9c275557054b34fe890d8e0cf7e86a5bc7b
iwasgodonce <ska201291@gmail.com>
2022-07-20 22:49:06 +0300

    ConvertToBool

--- Firstapp/Program.cs
+++ Firstapp/Program.cs
@@ -2,8 +2,13 @@
 {
     private static void Main(string[] args)
     {
-        byte number = 254;
-        long number1 = Convert.ToInt64(number);
-        Console.WriteLine(number1);
+        //byte number = 254;
+        //long number1 = Convert.ToInt64(number);
+        //Console.WriteLine(number1);
+
+        Console.WriteLine("Обезьяны любят бананы");
+        string answer = Console.ReadLine();
+        bool result = Convert.ToBoolean(answer);
+        Console.WriteLine(result);
     }
 }
